6f2148e23d4694b211bab4361640856c24d5f3af
linton430 <lintonsterling30@gmail.com>
2024-12-17 11:37:33 +0000

    Add files via upload

---
+++ Modular code.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("sum", num1 + num2)
+print("difference", num1 - num2)
+print("product", num1 * num2)
+print("Quotient", num1 / num2)
+for result in results:
+    print(result)
+    for result in results:
+        file.write(result + "/n")
+    
---
+++ buggy_code.py
@@ -0,0 +1,11 @@
+def extract_and_rearrange(string):
+    str_1 = "".join(reversed(string[0:4].split('g'))).capatalise()
+    str_2 = "".join(string[6:13.split('ro'))
+    str_3 = "".join(reversed(list(string[14:20]))).split(string[17])[0]
+    str_4 = "".join(string[21:29].split(string[23:28]))
+#simeple syntax errors such as missing colons and typo on split
+    return str_1 + " " + str_2 " " + str_3 + " " + str_4
+#incorrect function names
+def ultra_extacr_and_rearrange(string):
+    return first_transform
+print(ultra_extract_and_rearrange("corrected the code i hope !"))
